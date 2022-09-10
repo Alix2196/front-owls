55300e0ccd2bfa02ae6fecbd8dccee3e5cd6705e
Alix Meneses <angelica962008@hotmail.com>
2022-09-10 18:04:41 -0500

    primary proyect init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { CreateClientComponent } from './views/create-client/create-client.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  profileForm = new FormGroup({
+    name : new FormControl(''),
+    surname : new FormControl(''),
+    phone : new FormControl(''),
+    email : new FormControl(''),
+    password : new FormControl('')
+
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  createRegister(){
+    console.log(this.profileForm.value.name);
+  }
+
+}
